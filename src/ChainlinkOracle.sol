daa6812c2f5376810b9971c2fc897231b766bb38
yogesh0509 <agrawalyogesh5047@gmail.com>
2023-08-15 12:37:58 +0530

    added chainlink oracle

---
+++ src/ChainlinkOracle.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: AEL
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./IOracle.sol";
+
+contract ChainlinkOracle is IOracle {
+    AggregatorV3Interface internal dataFeed;
+    uint256 public immutable linkDecimals;
+    uint256 public immutable decimals;
+
+    constructor(address _dataFeedAddress, uint256 _linkDecimals, uint256 _decimals) {
+
+        dataFeed = AggregatorV3Interface(_dataFeedAddress);
+        linkDecimals = _linkDecimals;
+        decimals = _decimals;
+    }
+
+    function acceptTermsOfService() external {}
+
+    function readData() external view returns (uint256) {
+
+        (, int answer,,,) = dataFeed.latestRoundData();
+        require(answer >= 0, "Cannot convert negative value");
+        return (uint256(int256(answer)) * decimals / linkDecimals);
+    }
+}
