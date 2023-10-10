6c78dfe19cd22aea67b007980dc136c68d627d8f
Yogesh Agrawal <agrawalyogesh5047@gmail.com>
2023-10-10 15:28:28 +0530

    added Chainlink oracles (#10)
    This is a pull request for adding chainlink oracles!!

---
+++ scripts/deployChainlinkOracle.sh
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env bash
+
+if [ -f .env ]
+then
+  export $(cat .env | xargs) 
+else
+    echo "Please set your .env file"
+    exit 1
+fi
+
+
+forge create --legacy --rpc-url ${RPC_URL} \
+    --constructor-args  ${DATA_FEED_ADDRESS} ${DJED_DECIMALS} \
+    --private-key ${PRIVATE_KEY} src/ChainlinkOracle.sol:ChainlinkOracle
---
+++ src/ChainlinkOracle.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AEL
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./IOracle.sol";
+
+contract ChainlinkOracle is IOracle {
+    AggregatorV3Interface internal dataFeed;
+    uint256 public immutable scalingFactor;
+
+    constructor(address _dataFeedAddress, uint256 _decimals) {
+        dataFeed = AggregatorV3Interface(_dataFeedAddress);
+        scalingFactor = 10 ** (_decimals - uint256(dataFeed.decimals()));
+    }
+
+    function acceptTermsOfService() external {}
+
+    function readData() external view returns (uint256) {
+
+        (, int answer,,,) = dataFeed.latestRoundData();
+        return (uint256(int256(answer)) * scalingFactor);
+    }
+}
