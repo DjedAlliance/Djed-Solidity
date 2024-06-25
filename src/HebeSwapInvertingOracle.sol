bf22cee9236fd7f6c2af96125bd744195cf9718f
Anshdeep Singh <a_singh09@yahoo.com>
2024-06-25 19:59:31 +0530

    [Feat] Added HebeSwap Oracle for Ethereum Classic (#29)
    Added HebeSwap Oracle for getting price feed for `ETC/USDT`.
    Deploy script has also been added.

---
+++ src/HebeSwapInvertingOracle.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AEL
+pragma solidity ^0.8.0;
+
+import "@hebeswap/IStdReference.sol";
+import "./IOracle.sol";
+
+contract HebeSwapInvertingOracle is IOracle {
+    IStdReference public ref;
+    uint256 public immutable scalingFactor;
+
+    constructor(IStdReference _ref, uint8 _decimals, uint8 _hebeSwapDecimals) {
+        ref = _ref;
+        scalingFactor = 10**(uint256(_decimals + _hebeSwapDecimals) );
+    }
+
+    function acceptTermsOfService() external {}
+
+    function readData() external view returns (uint256) {
+        IStdReference.ReferenceData memory data = ref.getReferenceData("ETC", "USDT");
+        return (scalingFactor) / uint256(int256(data.rate));
+    }
+}
