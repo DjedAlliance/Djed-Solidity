85157be45037a0beb21ddd57eb8f4d97ead6c40e
Anshdeep Singh <a_singh09@yahoo.com>
2024-07-22 16:10:02 +0000

    Changed hardcoded values to immutable variables in the constructor

--- src/HebeSwapOracle.sol
+++ src/HebeSwapOracle.sol
@@ -7,16 +7,20 @@ import "./IOracle.sol";
 contract HebeSwapOracle is IOracle {
     IStdReference public immutable ref;
     uint256 public immutable scalingFactor;
+    string public immutable baseToken;
+    string public immutable quoteToken;
 
-    constructor(IStdReference _ref, uint8 _decimals, uint8 _hebeSwapDecimals) {
+    constructor(IStdReference _ref, uint8 _decimals, uint8 _hebeSwapDecimals, string memory _baseToken, string memory _quoteToken) {
         ref = _ref;
         scalingFactor = 10**(uint256(_decimals - _hebeSwapDecimals) );
+        baseToken = _baseToken;
+        quoteToken = _quoteToken;
     }
 
     function acceptTermsOfService() external {}
 
     function readData() external view returns (uint256) {
-        IStdReference.ReferenceData memory data = ref.getReferenceData("ETC", "USDT");
+        IStdReference.ReferenceData memory data = ref.getReferenceData(baseToken, quoteToken);
         return (uint256(int256(data.rate)) * scalingFactor);
     }
 }
