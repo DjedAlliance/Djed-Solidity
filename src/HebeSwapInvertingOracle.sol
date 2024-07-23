bbbaf3d7532686a9bfcc4122d4cfe61cbbf1a5e7
Anshdeep Singh <a_singh09@yahoo.com>
2024-07-24 01:25:20 +0530

    Made HebeSwap oracle variable as immutable (#33)

--- src/HebeSwapInvertingOracle.sol
+++ src/HebeSwapInvertingOracle.sol
@@ -5,18 +5,22 @@ import "@hebeswap/IStdReference.sol";
 import "./IOracle.sol";
 
 contract HebeSwapInvertingOracle is IOracle {
-    IStdReference public ref;
+    IStdReference public immutable ref;
     uint256 public immutable scalingFactor;
+    string public baseToken;
+    string public quoteToken;
 
-    constructor(IStdReference _ref, uint8 _decimals, uint8 _hebeSwapDecimals) {
+    constructor(IStdReference _ref, uint8 _decimals, uint8 _hebeSwapDecimals, string memory _baseToken, string memory _quoteToken) {
         ref = _ref;
         scalingFactor = 10**(uint256(_decimals + _hebeSwapDecimals) );
+        baseToken = _baseToken;
+        quoteToken = _quoteToken;
     }
 
     function acceptTermsOfService() external {}
 
     function readData() external view returns (uint256) {
-        IStdReference.ReferenceData memory data = ref.getReferenceData("ETC", "USDT");
+        IStdReference.ReferenceData memory data = ref.getReferenceData(baseToken, quoteToken);
         return (scalingFactor) / uint256(int256(data.rate));
     }
 }
