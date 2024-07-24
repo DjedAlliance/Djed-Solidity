4fdb7f8675bdfa2e0ded9be1f4647429aff0aef2
a-singh09 <a_singh09@yahoo.com>
2024-07-24 12:19:08 +0530

    feat: Add BASE_TOKEN and QUOTE_TOKEN to HebeSwap oracle deployment scripts

--- scripts/deployHebeSwapInvertingOracle.sh
+++ scripts/deployHebeSwapInvertingOracle.sh
@@ -11,5 +11,5 @@ fi
 
 
 forge create --legacy --rpc-url ${RPC_URL} \
-    --constructor-args  ${DATA_FEED_ADDRESS} ${DJED_DECIMALS} ${HEBESWAP_DECIMALS} \
+    --constructor-args  ${DATA_FEED_ADDRESS} ${DJED_DECIMALS} ${HEBESWAP_DECIMALS} ${BASE_TOKEN} ${QUOTE_TOKEN} \
     --private-key ${PRIVATE_KEY} src/HebeSwapInvertingOracle.sol:HebeSwapInvertingOracle
