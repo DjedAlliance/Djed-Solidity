b3f99d46aa7059fc68bbfb8e99280137acbecd45
Anshdeep Singh <a_singh09@yahoo.com>
2024-08-02 16:00:36 +0530

    Added support for ethereum classic mainnet (#35)

--- scripts/Helper.sol
+++ scripts/Helper.sol
@@ -6,7 +6,8 @@ contract Helper {
     enum SupportedNetworks {
         ETHEREUM_SEPOLIA,
         MILKOMEDA_TESTNET,
-        ETHEREUM_CLASSIC_MORDOR
+        ETHEREUM_CLASSIC_MORDOR,
+        ETHEREUM_CLASSIC_MAINNET
     }
 
     mapping(SupportedNetworks enumValue => string humanReadableName)
@@ -24,7 +25,9 @@ contract Helper {
     uint256 public TX_LIMIT;
 
     address constant CHAINLINK_SEPOLIA_INVERTED_ORACLE_ADDRESS = 0xB9C050Fd340aD5ED3093F31aAFAcC3D779f405f4;
-    address constant HEBESWAP_ORACLE_INVERTED_ADDRESS = 0xb0d99da21Bb4fa877e3D1DCA89E6657c5e840Eb2;
+    address constant HEBESWAP_ORACLE_INVERTED_ADDRESS_MORDOR = 0xb0d99da21Bb4fa877e3D1DCA89E6657c5e840Eb2;
+    address constant HEBESWAP_ORACLE_INVERTED_ADDRESS_MAINNET = 0x2fd961e20896e121EC7D499cC4F38462e286994A;
+    
     address oracleAddress;
     address treasuryAddress;
 
@@ -32,6 +35,7 @@ contract Helper {
         networks[SupportedNetworks.ETHEREUM_SEPOLIA] = "Ethereum Sepolia";
         networks[SupportedNetworks.MILKOMEDA_TESTNET] = "Milkomeda C1 Testnet";
         networks[SupportedNetworks.ETHEREUM_CLASSIC_MORDOR] = "Ethereum Classic Mordor";
+        networks[SupportedNetworks.ETHEREUM_CLASSIC_MAINNET] = "Ethereum Classic Mainnet";
     }
 
     function getConfigFromNetwork(
@@ -61,7 +65,22 @@ contract Helper {
         }
 
         if (network == SupportedNetworks.ETHEREUM_CLASSIC_MORDOR) {
-            oracleAddress = HEBESWAP_ORACLE_INVERTED_ADDRESS;
+            oracleAddress = HEBESWAP_ORACLE_INVERTED_ADDRESS_MORDOR;
+            treasuryAddress = 0x078D888E40faAe0f32594342c85940AF3949E666;
+            SCALING_FACTOR=1e24;
+            INITIAL_TREASURY_FEE=25e20;
+            TREASURY_REVENUE_TARGET=1e25;
+            RESERVE_RATIO_MIN=4e24;
+            RESERVE_RATIO_MAX=8e24;
+            FEE=15e21;
+            THREASHOLD_SUPPLY_SC=5e11;
+            RESERVE_COIN_MINIMUM_PRICE=1e18;
+            RESERVE_COIN_INITIAL_PRICE=1e20;
+            TX_LIMIT=1e10;
+        }
+
+        if (network == SupportedNetworks.ETHEREUM_CLASSIC_MAINNET) {
+            oracleAddress = HEBESWAP_ORACLE_INVERTED_ADDRESS_MAINNET;
             treasuryAddress = 0x078D888E40faAe0f32594342c85940AF3949E666;
             SCALING_FACTOR=1e24;
             INITIAL_TREASURY_FEE=25e20;
